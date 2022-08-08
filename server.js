2d0baa64afc13654fc6e8268933b54a552c86faa
indy6678 <indy6678@gmail.com>
2022-08-07 20:09:03 -0500

    added get and post to include unique id

--- server.js
+++ server.js
@@ -1,16 +1,31 @@
+const { json } = require('express');
 const express = require('express');
 const PORT = process.env.PORT || 80;
 const app = express();
-const db = require('./db/db.json');
+// parse incoming string or array data; must use with POST!!!
+app.use(express.urlencoded({extended: true}));
+// parse incoming JSON data; must use with POST!!!
+app.use(express.json());
+const notes = require('./db/db.json');
+const randnum = require('./utils/randnum');
 
-app.get('/db', (req, res) => {
-    let results = db;
+app.get('/api/notes', (req, res) => {
+    let results = notes;
     console.log(results);
     if(results) {
         res.json(results);
     }   else {
         res.sendStatus(404);
     }    
+})
+
+app.post('/api/notes', (req, res) => {
+    let results = notes
+    const mapped = results.map((element) => ({
+        ...element, id: randnum()
+    }));
+    console.log(mapped);
+    res.json(mapped);
 })
 
 app.listen(PORT, () => {
